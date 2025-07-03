2c8fc9d9ef3f6e8424e1496d935a4da16ab41471
Sahil Chess <125219792+sahilchess@users.noreply.github.com>
2025-07-03 18:01:07 +0000

    title

---
+++ main.py
@@ -0,0 +1,2 @@
+int(input("Welcome to Simple Project! \n\nChoose an application:\n1. Calculator\n2. Password Generator\n3. Dice Roller Simulator\n4. Countdown Timer\n5. Quizzler\n6. Stock Price Checker\n7. Hangman\n8. Roman Numeral Converter"))
+
